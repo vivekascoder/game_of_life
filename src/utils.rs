2ce1911283e7d71480cce8893c34436d59a2170a
vivekascoder <vivekascoder@gmail.com>
2023-06-23 20:13:52 +0530

    feat: can set/unset the cell now.

---
+++ src/utils.rs
@@ -0,0 +1,12 @@
+use bevy::prelude::*;
+
+pub const TILE_SIZE: f32 = 5.0;
+pub const NUMBER_OF_TILES: f32 = 100.0;
+pub const TOTAL_CELL: f32 = TILE_SIZE * NUMBER_OF_TILES;
+
+pub fn get_cell_from_position(position: Vec2) -> (f32, f32) {
+    (
+        (position.x / TILE_SIZE).floor() as f32,
+        (position.y / TILE_SIZE).floor() as f32,
+    )
+}
